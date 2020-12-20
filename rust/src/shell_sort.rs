d847dfee3b9e5fc7af9d8f8281a096be69158633
Elijah Voigt <elijah.caine.mv@gmail.com>
2020-12-20 15:15:03 -0800

    exercise(shell sort): first pass implementation, mostly fails
    I think I need to adjust how my windowing works.
    Currently my lists are getting _more_ sorted, but not _fully_ sorted.
    Some lower elements get near the higher end of the list.
    
    My theory that my nieve gap = len(input) / 3 + 1 is probably wrong.

--- rust/src/shell_sort.rs
+++ rust/src/shell_sort.rs
@@ -13,8 +13,61 @@
 /// type T where T implements PartialOrd (or something like that), but for simplicity we're using
 /// a concrete integer type.
 ///
+/// Following thes resources:
+/// * https://www.tutorialspoint.com/data_structures_algorithms/shell_sort_algorithm.htm
+/// * https://www.toptal.com/developers/sorting-algorithms/shell-sort
+///
+/// The algorithm goes like this:
+/// Step 1 − Initialize the value of h
+/// Step 2 − Divide the list into smaller sub-list of equal interval h
+/// Step 3 − Sort these sub-lists using insertion sort
+/// Step 3 − Repeat until complete list is sorted
+///
+/// Wikipedia has a good high-level description:
+///
+/// > The method starts by sorting pairs of elements far apart from each other, then progressively
+/// > reducing the gap between elements to be compared. By starting with far apart elements, it can
+/// > move some out-of-place elements into position faster than a simple nearest neighbor exchange.
+///
+/// We are ignoring a lot of easy performance gains, for example:
+///
+/// * Sorting `input` in place. We duplicate it and return a whole new vector. This is memory
+///   inefficient at the very least.
+///
 pub fn sort(input: &Vec<u8>) -> Vec<u8> {
-    todo!()
+    let mut output: Vec<u8> = input.clone();
+
+    // Initialize `gap` to be length of the input array.
+    // This immediately gets cut to ~1/3 for faster comparison.
+    // We cannot initialize it to 1/3 outside of the loop because of the `while` comparison.
+    let mut gap = output.len();
+
+    while gap != 1 {
+        // We start with a gap of len/3 + 1
+        // Tutorials/psuedocode complicate this step...
+        // I'm still not sure why we cannot statically start with this gap.
+        // And that makes me nervous.
+        gap = gap / 3 + 1;
+
+        println!("DEBUG gap: {}", gap);
+
+        for index in gap..output.len() {
+            println!("DEBUG {:?}", output);
+            // `a` is the left-most value in our tuple comparison
+            let a = index-gap;
+            // `b` is our right-most value in our tuple comparison
+            let b = index;
+            println!("DEBUG comparing output[{}] ({}) > output[{}] ({})", a, output[a], b, output[b]);
+            // If the left value is bigger, swap them
+            if output[a] > output[b] {
+                println!("DEBUG Swapping output[{}] {} and output[{}] {}", a, output[a], b, output[b]);
+                output.swap(a, b);
+            }
+        }
+        println!("DEBUG After iteration 1: {:?}", output);
+    }
+
+    output
 }
 
 #[test]
@@ -25,9 +78,11 @@ fn test_shell_sort() {
 
     let distr = rand::distributions::Uniform::new_inclusive(1, 100);
 
-    let input = (1..256).map(|_| rng.sample(distr)).collect::<Vec<u8>>();
+    let input = (1..10).map(|_| rng.sample(distr)).collect::<Vec<u8>>();
+    println!("DEBUG input: {:?}", input);
 
     let output = sort(&input);
+    println!("DEBUG input: {:?}", output);
 
     let mut sorted = input.clone();
     sorted.sort();
